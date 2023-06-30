eeda21faecb354c0b489755ad1e14534a03a274e
Pawan Rai <pawanr@LT-MBP-031.local>
2023-06-30 15:03:32 +0530

    updated sha and version

--- Formula/lambdatest-tunnel.rb
+++ Formula/lambdatest-tunnel.rb
@@ -1,27 +1,46 @@
-class LambdatestTunnel < Formula
-  # Fetching Sytem Architecture
-  arch = Hardware::CPU.is_64_bit? ? "64bit" : "32bit"
+require 'net/http'
+require 'json'
 
+class TunnelConfig
+  def initialize()
+    uri = URI('https://ts.lambdatest.com/v1.0/tunnel/config')
+    response = Net::HTTP.get_response(uri)
+    data = JSON.parse(response.body)
+    @version = data['config']['client_version']
+    @sha256 =  data['config']['mac_client_hash']
+  end
+
+  def getSha256
+    @sha256
+  end
+
+  def version
+    @version
+  end
+end
+
+class LambdatestTunnel < Formula
+
+  # Fetching Sytem Architecture
+  config = TunnelConfig.new()
   desc "Package for LambdaTest Tunnel"
   homepage "https://automation.lambdatest.com"
-  url "https://downloads.lambdatest.com/tunnel/v3/mac/#{arch}/LT_Mac.zip"
-  version "3.1.31"
-  sha256 "87c34de6a80ede137e0327fb4ee25ce06b0a7be1ee6d51ddda86d0dd6450fb8f"
+  url "https://downloads.lambdatest.com/tunnel/v3/mac/64bit/LT_Mac.zip"
+  version config.version
   license "Apache-2.0"
+  sha256 config.getSha256
 
   def install
-    arch = Hardware::CPU.is_64_bit? ? "64bit" : "32bit"
-    system "curl", "-L", "-o", "Lt_mac", "https://downloads.lambdatest.com/tunnel/v3/mac/#{arch}/LT_Mac.zip"
+    system "curl", "-L", "-o", "Lt_mac", "https://downloads.lambdatest.com/tunnel/v3/mac/64bit/LT_Mac.zip"
     # Unzipping binary
     system "unzip", "Lt_mac", "-d", bin.to_s
-
     mv "#{bin}/LT", "#{bin}/lambdatest-tunnel"
 
     # Making binary executable
     chmod "+x", "#{bin}/lambdatest-tunnel"
 
     # Installation success message
-    ohai "LambdaTest Tunnel is installed!"
+    puts "LambdaTest Tunnel is installed!"
     puts "To run the tunnel, use the following command:"
     puts "lambdatest-tunnel --user <name> -key <accesskey>"
   end
